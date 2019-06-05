f3c60e59afe8611540673393f055156c76f9934a
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-04 22:22:31 -0500

    Created get and post routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express(); 
+var controllers = require('./controllers.js');
 
 var port = 3000; 
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/todos', controllers.getAll());
+app.post('/todos', controllers.storeOne());
+
+
 
 app.listen(port, () => {console.log('listening on port 3000')});
