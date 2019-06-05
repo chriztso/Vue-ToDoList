d045ba27b251d11093448e9449852760122e5bd4
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-04 23:50:18 -0500

    Created adding tasks functionality

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <div class = 'enterBar'>
-      <input type = 'text'>
-    </div>
-    <div v-for = 'todo in todos'>
-       <div> {{todo.task}} </div>
-    </div>
+  <div class = 'wholeApp'>
+    <div class = 'listBody'>  
+        <div class = 'title'>
+            To-Do List
+        </div>      
+        <div class = 'enterBar'>
+          <input type = 'text' v-model= 'tasks'>
+          <input type = 'submit' value = 'Add task' v-on:click = 'addToDo'> 
+        </div>
+        <div v-for = 'todo in todos'>
+           <div> {{todo.task}} </div>
+        </div>
+    </div>    
   </div>
 </template>
 
@@ -14,12 +20,34 @@
 export default{
   data(){
     return {   
+      tasks : '',  
       todos : [   
           
       ]
     }  
+  },
+  methods : {
+    addToDo(){
+        var task = {task : this.tasks};
+        this.todos.push(task);
+        this.tasks = '';
+    }
   }
 }
 </script>
 
+<style>
+.wholeApp{
+    background-color: lightblue;
+    height: 100vw;
+}
 
+.listBody{
+    position: relative; 
+    left: 45%;
+}
+.title{
+    font-size: 30px;
+    color: white;
+}
+</style>
