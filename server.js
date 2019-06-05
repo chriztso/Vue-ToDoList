3ce79773eb935f80e752d488360fc6347c2cc8da
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-04 22:52:44 -0500

    Added body parser and fixed controller functions

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express(); 
 var controllers = require('./controllers.js');
+var bodyParser = require('body-parser');
 
 var port = 3000; 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/todos', controllers.getAll());
-app.post('/todos', controllers.storeOne());
+app.get('/todos', controllers.getAll);
+app.post('/todos', controllers.storeOne);
 
 
 
