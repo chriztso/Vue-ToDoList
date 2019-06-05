53796fd86e0d9e25651768930a875ef7759dffeb
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-04 22:52:18 -0500

    Fixed function name

--- controllers.js
+++ controllers.js
@@ -1,5 +1,5 @@
 var getToDos = require('./database.js').getTodos;
-var storeToDo = require('./database.js').storeTodo;
+var storeToDo = require('./database.js').storeToDo;
 
 var getAll = (req, res) => {
    getToDos((err, data) => {
@@ -12,13 +12,14 @@ var getAll = (req, res) => {
 }
 
 var storeOne = (req, res) => {
-    storeToDo((req.body.todo, (err, data) => {
+    console.log(req.body);
+    storeToDo(req.body.todo, (err, data) => {
         if(err){
             res.status(404).send(err);
             return;
         }
         res.status(201).send(data);
-    }))
+    })
 }
 
 exports.getAll = getAll; 
