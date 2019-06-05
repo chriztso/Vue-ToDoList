62fb4e718255fb9ff1ff2ca9d803931987b69d66
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-04 23:15:40 -0500

    Created list skeleton

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,1 +1,25 @@
+<template>
+  <div>
+    <div class = 'enterBar'>
+      <input type = 'text'>
+    </div>
+    <div v-for = 'todo in todos'>
+       <div> {{todo.task}} </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return {   
+      todos : [   
+          
+      ]
+    }  
+  }
+}
+</script>
+
 
